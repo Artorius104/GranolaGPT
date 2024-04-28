c33d5c5a599030fca0c30eda775b24174de486f9
Artorius104 <divin.badiabo@gmail.com>
2024-04-28 21:15:17 +0200

    [SCRAP] fix scrapping + happy images

--- data/src/spider/spider.py
+++ data/src/spider/spider.py
@@ -13,15 +13,17 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 # ENV Variables : URLs des pages à scrap + mood
+# Paths env variables
+absolute_path = os.path.dirname(__file__)
+res_path = os.path.join(absolute_path, "../../res")
+
+# URLs env variables
 happy_getty_page_url = "https://www.gettyimages.fr/photos/portrait-sourire?assettype=image&phrase=portrait%20sourire&sort=mostpopular&license=rf%2Crm"
 neutral_getty_page_url = "https://www.gettyimages.fr/photos/portrait-sans-expression?assettype=image&phrase=portrait%20sans%20expression&sort=mostpopular&license=rf%2Crm"
 sad_getty_page_url = "https://www.gettyimages.fr/photos/portrait-triste?assettype=image&phrase=portrait%20triste&sort=mostpopular&license=rf%2Crm"
 happy_google_page_url = "https://www.google.com/search?q=photo+portrait+sourire&sca_esv=4ec8c5ee11c012e4&rlz=1C1CHBF_frFR1089FR1089&udm=2&biw=958&bih=1078&sxsrf=ACQVn0-czAMsrZBzi2iU5nsLoyAeW59zCg%3A1714189767680&ei=x3UsZr2SKZ-okdUP55CcoAo&ved=0ahUKEwj9kbjBvuGFAxUfVKQEHWcIB6QQ4dUDCBA&uact=5&oq=portrait+sourire&gs_lp=Egxnd3Mtd2l6LXNlcnAiEHBvcnRyYWl0IHNvdXJpcmUyBRAAGIAEMgUQABiABDIGEAAYCBgeSKA7ULoQWP85cAF4AJABAJgBNaABmgWqAQIxNrgBA8gBAPgBAZgCEKAC0AXCAgQQIxgnwgIKEAAYgAQYQxiKBcICCBAAGIAEGLEDwgINEAAYgAQYsQMYQxiKBcICBBAAGB7CAgYQABgFGB7CAgcQABiABBgYmAMAiAYBkgcCMTagB-lN&sclient=gws-wiz-serp"
 neutral_google_page_url = "https://www.google.com/search?q=photo+portrait+sans+expression&sca_esv=4ec8c5ee11c012e4&rlz=1C1CHBF_frFR1089FR1089&udm=2&biw=958&bih=1078&sxsrf=ACQVn0-q0CU5qbyrk0IL2F0vWzSLXCAVZQ%3A1714189777003&ei=0HUsZrnvPIKfkdUPzqSAOA&ved=0ahUKEwi5l_HFvuGFAxWCT6QEHU4SAAcQ4dUDCBA&uact=5&oq=portrait+sans+expression&gs_lp=Egxnd3Mtd2l6LXNlcnAiGHBvcnRyYWl0IHNhbnMgZXhwcmVzc2lvbkj6swJQ9oICWNqxAnALeACQAQCYAUigAccHqgECMjO4AQPIAQD4AQGYAhigAuQEwgIKEAAYgAQYQxiKBcICBRAAGIAEwgIGEAAYCBgewgIEEAAYHsICBBAjGCfCAggQABiABBixA8ICBBAAGAPCAgYQABgFGB6YAwCIBgGSBwIyNKAHnkg&sclient=gws-wiz-serp"
 sad_google_page_url = "https://www.google.com/search?q=photo+portrait+triste+couleur&sca_esv=4ec8c5ee11c012e4&rlz=1C1CHBF_frFR1089FR1089&udm=2&biw=958&bih=1078&sxsrf=ACQVn0_arSQYgVtLtugkf5BZqWSoK4965A%3A1714189873147&ei=MXYsZrrQCJP2kdUPmYmx-AM&ved=0ahUKEwj6rN3zvuGFAxUTe6QEHZlEDD8Q4dUDCBA&uact=5&oq=portrait+triste+couleur&gs_lp=Egxnd3Mtd2l6LXNlcnAiF3BvcnRyYWl0IHRyaXN0ZSBjb3VsZXVySPIVUKsDWMMUcAF4AJABAJgBK6ABvQKqAQE4uAEDyAEA-AEBmAIDoAJWwgIKEAAYgAQYQxiKBcICBRAAGIAEwgIGEAAYCBgewgIEEAAYHpgDAIgGAZIHATOgB88I&sclient=gws-wiz-serp"
-happy = "happy"
-neutral = "neutral"
-sad = "sad"
 
 
 # SCRAPPING
@@ -49,14 +51,13 @@ def scrapping_images(driver, source: str, page_url: str) -> list[dict[int, str]]
 
 
 # TELECHARGEMENT
-def download_images(element_list: list[dict[int, str]], mood: str) -> None:
-    path = mood + "_images"
+def download_images(element_list: list[dict[int, str]], path: str) -> None:
     for index, img_info in enumerate(element_list):
         img_url = img_info['url']
         response = requests.get(img_url)
 
         if response.status_code == 200:
-            with open(f'{path}/face_{index}.jpg', 'wb') as f:
+            with open(f'{path}/scrapped_{index}.jpg', 'wb') as f:
                 f.write(response.content)
                 print(f"Image {index} téléchargée avec succès.")
         else:
@@ -64,9 +65,12 @@ def download_images(element_list: list[dict[int, str]], mood: str) -> None:
 
 
 def main():
+    # Mettre la catégorie des images à scrapper : happy, sad, neutral
+    mood = "happy"
+    path = res_path + '/' + mood
     # Création du répertoire happy_images si non existant
-    # if not os.path.exists('../res/happy_images'):
-    #     os.makedirs('../res/happy_images')
+    if not os.path.exists(path):
+        os.makedirs(path)
 
     # Série d'option nécessaire au fonctionnement de Selenium (à voir pourquoi)
     options = Options()
@@ -78,7 +82,7 @@ def main():
     # Liste de dictionnaires pour stocker les URLs
     element_list = scrapping_images(driver, "getty", happy_getty_page_url)
     print(element_list)
-    download_images(element_list, happy)
+    download_images(element_list, path)
 
     # Fermeture du driver
     driver.close()
